a1f1b6c82142d405d4ae50c426e4930f65020bcd
doev03 <doev03@gmail.com>
2023-11-01 20:59:43 +0300

    Fix paths

--- src/routes/order/+page.svelte
+++ src/routes/order/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import { base } from '$app/paths';
   import { fade, fly } from 'svelte/transition';	
 </script>
 
 <section transition:fade >
   <h1>Об этом сайте</h1>
   <p>СДЕЛАТЬ...</p>
-  <a href="/">Главная</a>
+  <a href="{base}">Главная</a>
 </section>
